557ef17ee4992ab24398deee61944db1ff90eadc
Danielle Thompson <danithompson74@gmail.com>
2020-12-07 11:27:36 -0800

    Basic cs structure w/ console taking usernumber
    Co-authored-by: Ben McFarland <benrmcfarland@gmail.com>

---
+++ PingPong.cs
@@ -0,0 +1,19 @@
+using System;
+
+class PingPong
+{
+  static void Main()
+  {
+    Console.WriteLine("Give me a number and I will return all numbers up to the number you provide, in ping-pong format.");
+    string yourStringNumber = Console.ReadLine();
+    int yourNumberToInt = int.Parse(yourStringNumber);
+
+  // use a list vs array 
+  // foreach (# % 3)
+
+  // foreach (# % 5)
+
+  // foreach (# % 3 && 5)
+  }
+
+}
